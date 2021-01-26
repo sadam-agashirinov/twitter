1bf74f0645b632d41cad7d3e5e5f6dea995ca026
sadam-agashirinov <sadam.agashirinov@gmail.com>
2021-01-26 19:58:27 +0300

    Добавлен контроллер авторизации и регистрации аккаунта

---
+++ TwitterApi/TwitterApi.Core/Contracts/Common/ApiRouters.cs
@@ -0,0 +1,30 @@
+﻿namespace TwitterApi.Core.Contracts.Common
+{
+    /// <summary>
+    /// Маршруты для запросов
+    /// </summary>
+    public static class ApiRouters
+    {
+        public const string Root =  "api";
+        public const string Base = Root + "/" + "/[controller]";
+
+        /// <summary>
+        /// Маршруты для методов авторизации
+        /// </summary>
+        public static class Account
+        {
+            /// <summary>
+            /// Регистрация пользователя
+            /// </summary>
+            public const string Registration = Base + "/registration";
+            /// <summary>
+            /// Авторизация пользователя
+            /// </summary>
+            public const string Authenticate = Base + "/authenticate";
+            /// <summary>
+            /// Обновление токена
+            /// </summary>
+            public const string RefreshToken = Base + "/refreshtoken";
+        }
+    }
+}
---
+++ TwitterApi/TwitterApi.Core/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TwitterApi.Core.Contracts.Common;
+using TwitterApi.DataLayer.Common;
+
+namespace TwitterApi.Core.Controllers
+{
+    /// <summary>
+    /// Контроллер авторизации и регистрации аккаунта
+    /// </summary>
+    [ApiController]
+    [AllowAnonymous]
+    [Produces("application/json")]
+    public class AccountController : ControllerBase
+    {
+        private TwitterDbContext _dbContext;
+        
+        /// <summary>
+        /// Контроллер авторизации и регистрации аккаунта
+        /// </summary>
+        public AccountController(TwitterDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Регистрация нового пользователя
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost(ApiRouters.Account.Registration)]
+        public Task<ActionResult> Registration()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Авторизация пользователя
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost(ApiRouters.Account.Authenticate)]
+        public Task<ActionResult> Authenticate()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Обновления рефреш токена
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost(ApiRouters.Account.RefreshToken)]
+        public Task<ActionResult> RefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
